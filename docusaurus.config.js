7623ecce667a23ea15ca7a90a055a2bd4125de90
sumatrabean <kennyho.code@gmail.com>
2023-01-28 22:25:35 -0500

    config gh action

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,7 +5,7 @@ const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 const organizationName = 'kennyho-code'
-const projectNAme = 'kenny-ho-docs'
+const projectName = 'kenny-ho-docs'
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
